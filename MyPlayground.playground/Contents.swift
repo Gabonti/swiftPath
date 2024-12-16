228c900a8e41b50f25fb65a48fe46f133484d33f
gabonti <gabdilkarim05@mail.ru>
2024-12-16 09:27:21 +0500

    did 3 subtasks from first task

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,52 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// MARK: Constants, variables and data types
+// Exercise 1
+//var intVar = 0
+//var uIntVar: UInt = 0
+//var floatVar: Float = 0.0
+//var doubleVar: Double = 0.0
+//var strVar = "Nothing"
+//
+//let intConst = 1
+//let uIntConst: UInt = 2
+//let floatConst: Float = 3.0
+//let doubleConst: Double = 4.0
+//let strConst = "Something"
+//
+//print("Int: min = \(Int.min), max = \(Int.max)")
+//print("UInt: min = \(UInt.min), max = \(UInt.max)")
+//print("Float: min = \(Float.leastNormalMagnitude), max = \(Float.greatestFiniteMagnitude)")
+//print("Double: min = \(Double.leastNormalMagnitude), max = \(Double.greatestFiniteMagnitude)")
 
-var myName = "Gaba"
+// Exercise 2
+//typealias Product = (name: String, quantity: Int)
+//
+//let products: [Product] = [
+//    (name: "Eggs", quantity: 15),
+//    (name: "Cucumber", quantity: 5),
+//    (name: "Banana", quantity: 2),
+//    (name: "Milk", quantity: 1),
+//    (name: "Bread", quantity: 2)
+//]
+//
+//for product in products {
+//    print("Name: \(product.name), Quantity: \(product.quantity)")
+//}
+
+// Exercise 3
+//let int = 1
+//let double = Double(int)
+//print("Int \(int) => Double \(double)")
+//
+//let str = "123"
+//let int1 = Int(str) ?? 0
+//if int1 == 0 {
+//    print("Error")
+//} else {
+//    print("String \(str) => Int \(int1)")
+//}
+//
+//let int2 = 100
+//let str1 = String(int2)
+//print("Int \(int2) -> String \(str1)")
