26516138481922037f1915ba049aa2ac35ba3188
gabonti <gabdilkarim05@mail.ru>
2024-12-17 07:10:02 +0500

    did 1 subtask from first task and 5 subtasks from second task

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -50,3 +50,66 @@ import UIKit
 //let int2 = 100
 //let str1 = String(int2)
 //print("Int \(int2) -> String \(str1)")
+
+// Exercise 4
+//let firstNum = 3
+//let secondNum = 2
+//let oper = "/"
+//let addition = firstNum + secondNum
+//let multiplication = firstNum * secondNum
+//let division = Double(firstNum) / Double(secondNum)
+//let substraction = firstNum - secondNum
+//
+//switch oper {
+//case "+":
+//    print("\(firstNum) + \(secondNum) = \(addition)")
+//case "*":
+//    print("\(firstNum) * \(secondNum) = \(multiplication)")
+//case "/":
+//    print("\(firstNum) / \(secondNum) = \(division)")
+//case "-":
+//    print("\(firstNum) - \(secondNum) = \(substraction)")
+//default:
+//    print("Error")
+//}
+
+//Exercise 5
+// Will do it in the future. Exercise about readLine()
+
+
+// MARK: String
+// Exercise 1 & 2
+//let firstName = "Gaba"
+//let secondName = "Ata"
+//let age = 20
+//let city = "Almaty"
+//let hobby = "Training at the gym"
+//
+//print("My name is \(firstName + " " + secondName), I'm \(age) y.o. I live in \(city). My hobby is \(hobby)")
+
+// Exercise 3
+//let firstStr = "one"
+//let secondStr = "two"
+//let thirdStr = "three"
+//let forthStr = "four"
+//let fifthStr = "five"
+//let sixthStr = "six"
+//let seventhStr = "seven"
+//let eighthStr = "eight"
+//let ninthStr = "nine"
+//let tenthStr = "ten"
+//
+//print("\(firstStr), \(secondStr), \(thirdStr)\n\(forthStr), \(fifthStr), \(sixthStr)\n\(seventhStr), \(eighthStr), \(ninthStr)\n\t\(tenthStr)")
+
+// Exercise 4
+//let name = "Karim"
+//for l in name {
+//    print(l)
+//}
+
+
+// Exercise 5
+//let int = 5
+//let str = "apples"
+//
+//print(String(int) + " " + str)
